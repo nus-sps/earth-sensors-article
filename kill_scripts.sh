f31500ed0370887e00b40d95c2c645102a5a7670
mingyi456 <mingyi456@hotmail.com>
2022-08-03 17:16:07 +0800

    Add more code

--- kill_scripts.sh
+++ kill_scripts.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/sh
-prep -a python3 measurements.py
+if (pgrep -a python3 measurements.py | wc -l)
+	pkill python3
+	echo "Measurements.py terminated"
+	else
+		echo "Measurements.py is already not running"
 
