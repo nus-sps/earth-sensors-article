2cbb30e76d20f51ca836d7ccdb8b6b09b737003e
mingyi456 <mingyi456@hotmail.com>
2022-08-04 20:54:10 +0800

    Update shell scripts

--- kill_scripts.sh
+++ kill_scripts.sh
@@ -2,6 +2,7 @@
 if pgrep -f "python3 measurements.py" > /dev/null ; then
 	echo "A running measurements.py process has been detected. Terminating ..."
 	pkill -f -SIGTERM "python3 measurements.py"
+	sleep 2
 	if ! pgrep -f "python3 measurements.py" > /dev/null ; then
 		echo "measurements.py terminated successfully"
 	else
--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
+sudo pip3 install -r requirements.txt
+cat crontab.txt | crontab -
+echo "Done"
