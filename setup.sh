ff7a025f7bee25199b2af7f326d1a492e07467de
mingyi456 <mingyi456@hotmail.com>
2022-08-04 23:42:07 +0800

    Update setup.sh with interface configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 sudo apt update && sudo apt upgrade
+sudo apt autoremove
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial 1
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_ssh 0
 sudo pip3 install -r requirements.txt
 cat crontab.txt | crontab -
 echo "Done"
