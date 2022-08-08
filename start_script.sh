e4a539258de936e28f2acc06b1616a5a937795da
Chen Mingyi <65432252+mingyi456@users.noreply.github.com>
2022-08-08 11:22:34 +0800

    Update `start_script.sh` to fix a bug

--- start_script.sh
+++ start_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 SCRIPT_NAME="SP3275_measurements.py"
-nohup python3 "$SCRIPT_NAME" > ~/log.txt 2> ~/errors.txt < /dev/null & 
+nohup python3 ~/earth-sensors-article/"$SCRIPT_NAME" > ~/log.txt 2> ~/errors.txt < /dev/null & 
 exit
