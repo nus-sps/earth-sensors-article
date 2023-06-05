5b5475fdee7d91f5e3006e4f15a98ddbe174ef71
Chen Mingyi <65432252+mingyi456@users.noreply.github.com>
2023-06-05 15:56:53 +0800

    Undo updates to copy.sh
    The previous change was bugged

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sh
+OUTPUT_FILE="SP3275 Sensor Readings.csv"
 while true
 do
-	df --output=target | grep /media/$USER | xargs -r -n 1 -d '\n' cp "/home/$USER/*.csv"
+	df --output=target | grep /media/$USER | xargs -r -n 1 -d '\n' cp "/home/$USER/$OUTPUT_FILE"
 	sleep 10
 done
