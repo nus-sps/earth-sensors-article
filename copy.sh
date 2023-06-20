33f4b9f96ed68dea62cf9e3dff61c68d7350c070
Chen Mingyi <65432252+mingyi456@users.noreply.github.com>
2023-06-20 14:16:05 +0800

    Fix copy.sh

--- copy.sh
+++ copy.sh
@@ -2,6 +2,6 @@
 OUTPUT_FILE="SP3275 Sensor Readings.csv"
 while true
 do
-	df --output=target | grep /media/$USER | xargs -r -n 1 -d '\n' cp "/home/$USER/$OUTPUT_FILE"
+	df --output=target | grep /media/pi | xargs -r -n 1 -d '\n' cp "/home/pi/$OUTPUT_FILE"
 	sleep 10
 done
